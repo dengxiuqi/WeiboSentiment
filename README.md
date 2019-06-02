20b9cdfdf1973b979ad513483cac4f65869d74c8
albert <dengxiuqi@163.com>
2019-06-03 00:24:12 +0800

    基于FastText的中文微博情感分析

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # WeiboSentiment
-基于FastText的中文微博情感分析
+基于FastText的中文微博情感分析  
+数据来源： https://github.com/dengxiuqi/weibo2018  
+#### 主要工作
+* 用FastText生成词向量
+* 结合Tf-Idf和词向量生成句向量
+* 分别用SVM和神经网络训练分类器
+
+#### 结果
+在测试集上均取得0.8+的正确率
---
+++ utils.py
@@ -0,0 +1,38 @@
+import jieba
+import re
+
+
+def tokenize(text):
+    """
+    带有语料清洗功能的分词函数
+    """
+    text = re.sub("\{%.+?%\}", " ", text)           # 去除 {%xxx%} (地理定位, 微博话题等)
+    text = re.sub("@.+?( |$)", " ", text)           # 去除 @xxx (用户名)
+    text = re.sub("【.+?】", " ", text)              # 去除 【xx】 (里面的内容通常都不是用户自己写的)
+    icons = re.findall("\[.+?\]", text)             # 提取出所有表情图标
+    text = re.sub("\[.+?\]", "IconMark", text)      # 将文本中的图标替换为`IconMark`
+
+    tokens = []
+    for k, w in enumerate(jieba.lcut(text)):
+        w = w.strip()
+        if "IconMark" in w:                         # 将IconMark替换为原图标
+            for i in range(w.count("IconMark")):
+                tokens.append(icons.pop(0))
+        elif w and w != '\u200b' and w.isalpha():   # 只保留有效文本
+                tokens.append(w)
+    return tokens
+
+
+def load_curpus(path):
+    """
+    加载语料库
+    """
+    data = []
+    with open(path, "r", encoding="utf8") as f:
+        for line in f:
+            [_, seniment, content] = line.split(",", 2)
+            content = tokenize(content)             # 分词
+            data.append((content, int(seniment)))
+    return data
+
+
