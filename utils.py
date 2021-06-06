5a7f915b58489cd890aafa24a841d5135e988031
AlbertDxq <dengxiuqi@163.com>
2021-06-07 01:31:53 +0800

    add processing_bert()

--- utils.py
+++ utils.py
@@ -30,6 +30,7 @@ def load_corpus_bert(path):
     with open(path, "r", encoding="utf8") as f:
         for line in f:
             [_, seniment, content] = line.split(",", 2)
+            content = processing_bert(content)
             data.append((content, int(seniment)))
     return data
 
@@ -54,3 +55,15 @@ def processing(text):
     # 用空格拼接成字符串
     result = " ".join(words)
     return result
+
+
+def processing_bert(text):
+    """
+    数据预处理, 可以根据自己的需求进行重载
+    """
+    # 数据清洗部分
+    text = re.sub("\{%.+?%\}", " ", text)           # 去除 {%xxx%} (地理定位, 微博话题等)
+    text = re.sub("@.+?( |$)", " ", text)           # 去除 @xxx (用户名)
+    text = re.sub("【.+?】", " ", text)              # 去除 【xx】 (里面的内容通常都不是用户自己写的)
+    text = re.sub("\u200b", " ", text)              # '\u200b'是这个数据集中的一个bad case, 不用特别在意
+    return text
